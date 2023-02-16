ea25e50d5a9ca928425561eb2a53f5a631f67b7f
Fredcie <obiomacalozie@gmail.com>
2023-02-15 22:37:44 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
-#!/bin/bash
-#INCLUDE <stdio.h>
+#include <stdio.h>
 /**
- * main -A program that prints a line with puts function
- n
- * Return: 0(Success)
- */
+*main - Entry point
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
-puts(“\”Programming is like building a multilingual puzzle
-“);
-return (0);
+        printf(“with proper grammar, but the outcome is a piece of art,\n”);
+        return (0);
 }
+~
+~
 ~
